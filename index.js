522a60212786d0f742cfabc32d0e054483426de7
Diana Orecchio <di.orecchio@gmail.com>
2017-09-14 19:39:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string.toLowerCase() === lowercase){
-    console.log ('I can\'t hear you!')
+    return ('I can\'t hear you!')
   } else if (string.toUpperCase() === uppercase) {
-    console.log ("YES INDEED!")
+    return ("YES INDEED!")
   } else { //"I love you, Grandma.")
-    console.log ("I love you, too.")
+    return ("I love you, too.")
   }
 }
 /*
