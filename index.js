4eb0d4c1214579061d726ac736ea34fe0cd265f8
Diana Orecchio <di.orecchio@gmail.com>
2017-09-14 19:20:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase ()
 }
-/* 
+/*
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
